53c34db53c4937105659d7443cc60ffcaabf225d
YukaiYuan <13588278219@163.com>
2021-05-27 20:04:10 +0800

    first commit

---
+++ src/test/java/com/yyk/iv/IvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyk.iv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
